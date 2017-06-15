0c2b713d7e3c35e8e73dd028fdcdb9b2cb0e5687
Jonas Arndt <mio991@gmx.de>
2017-06-15 11:48:09 +0200

    CLI 0.1

--- src/mio991/lgs/gui/CLI.java
+++ src/mio991/lgs/gui/CLI.java
@@ -1,16 +1,67 @@
 package mio991.lgs.gui;
 
-import mio991.math.Matrix;
+import java.util.*;
+import mio991.math.*;
 
 public class CLI {
+	
+	private static Matrix s_Matrix;
+	private static LGS s_linearSystem;
 
 	public static void main(String[] args) {
-		Matrix m = new Matrix(new double[][]{{2, 3, 4}, {2, 1, 2}});
-		m.save(System.out);
+		Scanner scanner = new Scanner(System.in);
 		
-		Matrix i = m.inverse();
+		System.out.println(
+				"########################################\n" +
+				"#    Linear System Solver v0.4 CLI     #\n" + 
+				"########################################\n" +
+				"\n" +
+				"For help type 'help'. ;-P"
+				);
 		
-		i.save(System.out);
+		boolean runing = true;
+		
+		while(runing)
+		{
+			System.out.println("\nPlease type in a command to execute:");
+			switch(scanner.nextLine().toLowerCase())
+			{
+			case "help":
+				System.out.println( "\n" +
+						"You can use these Commands:\n"+
+						"\thelp\tthis help Text\n" +
+						"\texit\texit this Programm"
+						);
+				break;
+			case "load":
+				System.out.println("What do you want to load? (MATRIX/system)");
+				switch(scanner.nextLine().toLowerCase())
+				{
+				case "":
+				case "matrix":
+					
+					break;
+				case "system":
+					
+					break;
+				default:
+					System.out.println("Sorry, I couldn't understand you.");
+					break;
+				}
+				break;
+			case "q":
+			case "exit":
+			case "quit":
+				runing = false;
+				break;
+			default:
+				System.out.println("Try 'help' for help.");
+				break;
+			}
+		}
+		
+		System.out.println("\nGoodbye!!!");
+		
+		scanner.close();
 	}
-
 }
